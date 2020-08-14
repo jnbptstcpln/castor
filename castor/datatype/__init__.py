8f2b278ef9b0efd8e1b18710a527bef57388de7b
Jean-Baptiste Caplan <jnbptstcpln@gmail.com>
2020-08-14 15:29:05 +0200

    Mise en place d'une fonction pour obtenir une valeur par défaut en fonction du type de données

--- castor/datatype/__init__.py
+++ castor/datatype/__init__.py
@@ -36,3 +36,37 @@ class Library:
             return getattr(module, component_name)
         except AttributeError:
             raise Exception("Aucun type de données nommé '"+component_name+"' au sein du module '"+module_id+"'")
+
+    @staticmethod
+    def default(id):
+
+        # Pour le type "*", on n'applique aucune forme de casting
+        if id == "*":
+            return None
+
+        builtin = {
+            'int': 0,
+            'float': 0.0,
+            'string': "",
+            'list': [],
+            'dict': {}
+        }
+
+        try:
+            return builtin[id]
+        except KeyError:
+            pass
+
+        module_part = id.split(".")
+        component_name = module_part.pop()
+        module_id = ".".join(module_part)
+
+        try:
+            module = importlib.import_module(module_id)
+        except ModuleNotFoundError:
+            raise Exception("Aucun module nommé '"+module_id+"'")
+
+        try:
+            return getattr(module, component_name)
+        except AttributeError:
+            raise Exception("Aucun type de données nommé '"+component_name+"' au sein du module '"+module_id+"'")
