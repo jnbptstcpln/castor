154791d15f030570f084b992d98e0e556daacf6f
Jean-Baptiste Caplan <jnbptstcpln@gmail.com>
2020-08-14 15:35:14 +0200

    - Ajout de la possiblité de changer la valeur d'un paramètres dans Setting

--- castor/helper.py
+++ castor/helper.py
@@ -10,6 +10,9 @@ class Setting:
         else:
             self.settings = {}
 
+    def set(self, key, value):
+        self.settings[key] = value
+
     def get(self, key, default=None):
         return self.settings.get(key, default)
 
